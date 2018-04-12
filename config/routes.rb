e5fde40356250edb87aa6f150f37d2b05a1fb673
pfcarey917 <longspeak917@gmail.com>
2018-04-12 18:24:30 -0500

    Log in, log out, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'admin/index'
+
+  #get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  root 'admin#index'
 end
